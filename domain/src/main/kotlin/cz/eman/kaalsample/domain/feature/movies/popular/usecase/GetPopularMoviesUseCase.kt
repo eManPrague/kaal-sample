f439b524202cc032021ecdf2363a3aa498bb25b9
vaclav.souhrada <vaclav.souhrada@eman.cz>
2019-05-30 15:09:51 +0200

    Removed unused code

--- domain/src/main/kotlin/cz/eman/kaalsample/domain/feature/movies/popular/usecase/GetPopularMoviesUseCase.kt
+++ domain/src/main/kotlin/cz/eman/kaalsample/domain/feature/movies/popular/usecase/GetPopularMoviesUseCase.kt
@@ -15,22 +15,5 @@ class GetPopularMoviesUseCase(private val moviesRepository: MoviesRepository) : 
 
     override suspend fun doWork(params: Unit): Result<List<Movie>> {
         return moviesRepository.getPopularMovies()
-        //return Result.Success(listOf(movie))
     }
-
-    private val movie = Movie(
-            adult = false,
-            backdropPath = "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
-            id = 420817,
-            originalTitle = "Aladdin",
-            originalLanguage = "en",
-            releaseDate = "2019-05-22",
-            posterPath = "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
-            popularity = 630.556,
-            title = "Aladdin",
-            voteAverage = 7.2,
-            voteCount = 538,
-            overview = "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
-            movieDetail = null
-    )
 }
