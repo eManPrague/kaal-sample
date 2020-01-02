d1e0692c17243c4800e9f867b8b9ee3715280a55
vaclav.souhrada <vaclav.souhrada@eman.cz>
2020-01-02 13:07:20 +0100

    🐛 Bottom Navigation Fix
    Fix a bottom navigation where setup with the navigation controller has been missing.

--- app/src/main/kotlin/cz/eman/kaalsample/presentation/feature/dashboard/DashboardActivity.kt
+++ app/src/main/kotlin/cz/eman/kaalsample/presentation/feature/dashboard/DashboardActivity.kt
@@ -2,13 +2,21 @@ package cz.eman.kaalsample.presentation.feature.dashboard
 
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import cz.eman.kaal.presentation.activity.BaseActivity
 import cz.eman.kaalsample.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 /**
  *  @author stefan.toth@eman.cz
  */
 class DashboardActivity : BaseActivity(R.layout.activity_dashboard) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        NavigationUI.setupWithNavController(bottomNav, findNavController(R.id.navHostFragment))
+    }
 
     override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
 }
