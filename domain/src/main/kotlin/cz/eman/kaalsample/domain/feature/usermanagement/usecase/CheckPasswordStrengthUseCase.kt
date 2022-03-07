154cb2417854f1af4c8cbbc790229a33d0d69ea5
dusan.micuda <dusan.micuda@eman.cz>
2022-03-07 09:39:45 +0100

    fixing CheckPasswordStrengthUseCase Suppress

--- domain/src/main/kotlin/cz/eman/kaalsample/domain/feature/usermanagement/usecase/CheckPasswordStrengthUseCase.kt
+++ domain/src/main/kotlin/cz/eman/kaalsample/domain/feature/usermanagement/usecase/CheckPasswordStrengthUseCase.kt
@@ -5,17 +5,16 @@ import cz.eman.kaalsample.domain.feature.usermanagement.model.PasswordStrength
 
 class CheckPasswordStrengthUseCase() : UseCase<PasswordStrength, String>() {
 
-    @Suppress("NewApi")
     override suspend fun doWork(params: String): PasswordStrength {
         val forbiddenChars = listOf("/", ",", " ")
         return when {
             forbiddenChars.any { it in params } -> PasswordStrength.Invalid
-            params.length >= 7 && (params.chars().anyMatch(Character::isUpperCase) &&
-                    params.chars().anyMatch(Character::isLowerCase) &&
-                    params.chars().anyMatch(Character::isDigit)) -> PasswordStrength.Strong
-            params.length >= 7 && (params.chars().anyMatch(Character::isUpperCase) ||
-                    params.chars().anyMatch(Character::isLowerCase) ||
-                    params.chars().anyMatch(Character::isDigit)) -> PasswordStrength.Medium
+            params.length >= 7 && (params.toCharArray().any(Character::isUpperCase) &&
+                    params.toCharArray().any(Character::isLowerCase)&&
+                    params.toCharArray().any(Character::isDigit)) -> PasswordStrength.Strong
+            params.length >= 7 && (params.toCharArray().any(Character::isUpperCase) ||
+                    params.toCharArray().any(Character::isLowerCase) ||
+                    params.toCharArray().any(Character::isDigit)) -> PasswordStrength.Medium
             params.isEmpty() -> PasswordStrength.Empty
             else -> PasswordStrength.Weak
         }
