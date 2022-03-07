f452f52961817741d6af5597b4e1b161be2cd725
dusan.micuda <dusan.micuda@eman.cz>
2022-03-07 16:22:17 +0100

    refactor CheckPasswordStrengthUseCaseTest to kotest

--- domain/src/test/kotlin/cz/eman/kaalsample/domain/CheckPasswordStrengthUseCaseTest.kt
+++ domain/src/test/kotlin/cz/eman/kaalsample/domain/CheckPasswordStrengthUseCaseTest.kt
@@ -1,34 +1,42 @@
 package cz.eman.kaalsample.domain
 
 import cz.eman.kaalsample.domain.feature.usermanagement.model.PasswordStrength
+import cz.eman.kaalsample.domain.feature.usermanagement.repository.SecurityRepository
 import cz.eman.kaalsample.domain.feature.usermanagement.usecase.CheckPasswordStrengthUseCase
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
-class CheckPasswordStrengthUseCaseTest {
+class CheckPasswordStrengthUseCaseTest : StringSpec() {
 
-    @Test
-    fun `password strength should be invalid`() {
-        val checkPasswordStrength = CheckPasswordStrengthUseCase()
-        runBlocking {
+    private val securityRepository = mockk<SecurityRepository>()
+
+    private val checkPasswordStrength = CheckPasswordStrengthUseCase(securityRepository)
+
+    init {
+        every { securityRepository.getForbiddenCharacters() } returns
+                listOf('/', ',', ' ', '<', '>')
+        every { securityRepository.getSuggestedCharacters() } returns
+                listOf('!', '@', '#', '?', '(', ')', '[', ']', '{', '}', '"', '$', '%',
+                    '&', '\'', '*', '+', '-', '.', ':', ';', '=', '\\', '^', '_', '`', '|', '~')
+
+        "password strength should be invalid" {
             listOf(
-                " ",
-                ",",
-                "/",
+                "bjkjh fhgd",
+                "as,dasad",
+                "fgawg/hzzdj",
                 "kjsdf,ksjdf",
-                "ijse kbse"
+                "ijse>kbse"
             ).forEach {
                 val result = checkPasswordStrength(it)
                 result shouldBe PasswordStrength.Invalid
             }
         }
-    }
 
-    @Test
-    fun `password strength should be empty`() {
-        val checkPasswordStrength = CheckPasswordStrengthUseCase()
-        runBlocking {
+        "password strength should be empty" {
             listOf(
                 ""
             ).forEach {
@@ -36,12 +44,8 @@ class CheckPasswordStrengthUseCaseTest {
                 result shouldBe PasswordStrength.Empty
             }
         }
-    }
 
-    @Test
-    fun `password strength should be weak`() {
-        val checkPasswordStrength = CheckPasswordStrengthUseCase()
-        runBlocking {
+        "password strength should be weak" {
             listOf(
                 "kjh",
                 "Khjf",
@@ -51,12 +55,8 @@ class CheckPasswordStrengthUseCaseTest {
                 result shouldBe PasswordStrength.Weak
             }
         }
-    }
 
-    @Test
-    fun `password strength should be medium`() {
-        val checkPasswordStrength = CheckPasswordStrengthUseCase()
-        runBlocking {
+        "password strength should be medium" {
             listOf(
                 "asdfghjk",
                 "qwerTyUiop",
@@ -67,12 +67,8 @@ class CheckPasswordStrengthUseCaseTest {
                 result shouldBe PasswordStrength.Medium
             }
         }
-    }
 
-    @Test
-    fun `password strength should be strong`() {
-        val checkPasswordStrength = CheckPasswordStrengthUseCase()
-        runBlocking {
+        "password strength should be strong" {
             listOf(
                 "ad@FkJ78",
                 "DfgG!5jkjh",
@@ -82,6 +78,83 @@ class CheckPasswordStrengthUseCaseTest {
                 result shouldBe PasswordStrength.Strong
             }
         }
+
     }
 
 }
+//
+//    @Test
+//    fun `password strength should be invalid`() {
+//        val checkPasswordStrength = CheckPasswordStrengthUseCase()
+//        runBlocking {
+//            listOf(
+//                " ",
+//                ",",
+//                "/",
+//                "kjsdf,ksjdf",
+//                "ijse kbse"
+//            ).forEach {
+//                val result = checkPasswordStrength(it)
+//                result shouldBe PasswordStrength.Invalid
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun `password strength should be empty`() {
+//        val checkPasswordStrength = CheckPasswordStrengthUseCase()
+//        runBlocking {
+//            listOf(
+//                ""
+//            ).forEach {
+//                val result = checkPasswordStrength(it)
+//                result shouldBe PasswordStrength.Empty
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun `password strength should be weak`() {
+//        val checkPasswordStrength = CheckPasswordStrengthUseCase()
+//        runBlocking {
+//            listOf(
+//                "kjh",
+//                "Khjf",
+//                "IUyH34"
+//            ).forEach {
+//                val result = checkPasswordStrength(it)
+//                result shouldBe PasswordStrength.Weak
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun `password strength should be medium`() {
+//        val checkPasswordStrength = CheckPasswordStrengthUseCase()
+//        runBlocking {
+//            listOf(
+//                "asdfghjk",
+//                "qwerTyUiop",
+//                "zxcvb76j5h",
+//                "hd4lFG6e"
+//            ).forEach {
+//                val result = checkPasswordStrength(it)
+//                result shouldBe PasswordStrength.Medium
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun `password strength should be strong`() {
+//        val checkPasswordStrength = CheckPasswordStrengthUseCase()
+//        runBlocking {
+//            listOf(
+//                "ad@FkJ78",
+//                "DfgG!5jkjh",
+//                "assd56HG#67"
+//            ).forEach {
+//                val result = checkPasswordStrength(it)
+//                result shouldBe PasswordStrength.Strong
+//            }
+//        }
+//    }
