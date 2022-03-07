d9fc0d1f4cd2c980e913b5b96119b0d4a31c81d5
Klariskao <klara.ond@seznam.cz>
2022-03-07 09:42:21 +0100

    Add icons to Password State

--- app/src/main/kotlin/cz/eman/kaalsample/presentation/feature/login/states/PasswordExtension.kt
+++ app/src/main/kotlin/cz/eman/kaalsample/presentation/feature/login/states/PasswordExtension.kt
@@ -8,30 +8,36 @@ fun PasswordStrength.toPasswordState() : PasswordStrengthState =
         is PasswordStrength.Invalid ->
             PasswordStrengthState(
                 R.string.password_strength_invalid,
-                R.color.password_strength_invalid
+                R.color.password_strength_invalid,
+                R.drawable.ic_baseline_cancel_24
             )
             is PasswordStrength.Empty ->
             PasswordStrengthState(
                 R.string.password_strength_empty,
-                R.color.password_strength_invalid
+                R.color.password_strength_invalid,
+                R.drawable.ic_baseline_cancel_24
             )
         is PasswordStrength.Weak ->
             PasswordStrengthState(
                 R.string.password_strength_weak,
-                R.color.password_strength_weak
+                R.color.password_strength_weak,
+                R.drawable.ic_baseline_cancel_24
             )
         is PasswordStrength.Medium ->
             PasswordStrengthState(
                 R.string.password_strength_medium,
-                R.color.password_strength_medium
+                R.color.password_strength_medium,
+                R.drawable.ic_baseline_cancel_24
             )
         is PasswordStrength.Strong ->
             PasswordStrengthState(
                 R.string.password_strength_strong,
-                R.color.password_strength_strong
+                R.color.password_strength_strong,
+                R.drawable.ic_baseline_check_circle_24
             )
         else -> PasswordStrengthState(
             R.string.password_strength_unknown,
-            R.color.password_strength_unknown
+            R.color.password_strength_unknown,
+            R.drawable.ic_baseline_cancel_24
         )
     }
--- app/src/main/kotlin/cz/eman/kaalsample/presentation/feature/login/states/PasswordStrengthState.kt
+++ app/src/main/kotlin/cz/eman/kaalsample/presentation/feature/login/states/PasswordStrengthState.kt
@@ -1,9 +1,11 @@
 package cz.eman.kaalsample.presentation.feature.login.states
 
 import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class PasswordStrengthState(
     @StringRes val errorTextId: Int,
-    @ColorRes val textColorId: Int
+    @ColorRes val textColorId: Int,
+    @DrawableRes val iconSourceId: Int
     )
